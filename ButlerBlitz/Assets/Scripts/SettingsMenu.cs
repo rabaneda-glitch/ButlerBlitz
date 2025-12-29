c693c99fde2469308c7a60d9fdf9ca65f749c367
SancenonA <asanbor@bbaa.upv.es>
2025-12-29 22:11:10 +0100

    Slider de sonido arreglado

--- ButlerBlitz/Assets/Scripts/SettingsMenu.cs
+++ ButlerBlitz/Assets/Scripts/SettingsMenu.cs
@@ -1,13 +1,43 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+
+    private const float k_MinSlider = 0.0001f;
+    private const string k_PlayerPrefKey = "VolumeMusic";
+
+    void Start()
+    {
+        float saved = PlayerPrefs.GetFloat(k_PlayerPrefKey, 1f);
+        SetVolume(saved);
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.minValue = 0f;
+            volumeSlider.maxValue = 1f;
+            volumeSlider.value = saved;
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (volumeSlider != null)
+            volumeSlider.onValueChanged.RemoveListener(SetVolume);
+    }
 
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("Volume", volume);
+        float clamped = Mathf.Clamp(volume, k_MinSlider, 1f);
+
+        if (audioMixer != null)
+            audioMixer.SetFloat("VolumeMusic", Mathf.Log10(clamped) * 20f);
+
+        PlayerPrefs.SetFloat(k_PlayerPrefKey, clamped);
     }
 
     public void SetFullscreen (bool isFullscreen)
