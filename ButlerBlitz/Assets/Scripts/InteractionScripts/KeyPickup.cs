9847a45b5d70cb6ba91fb878d97c1b2f38bd3d9a
ClaudiaRodenas559 <claudiarodenas559@gmail.com>
2026-01-03 09:13:42 +0100

    musica cinematica inicio

--- ButlerBlitz/Assets/Scripts/InteractionScripts/KeyPickup.cs
+++ ButlerBlitz/Assets/Scripts/InteractionScripts/KeyPickup.cs
@@ -30,7 +30,7 @@ public class KeyPickup : MonoBehaviour
 
             hasBeenPickedUp = true;
 
-            KeyTextManager.Instance.ShowTemporaryMessage("Llave para la biblioteca obtenida", 2f);
+            KeyTextManager.Instance.ShowTemporaryMessage("Llave para la biblioteca obtenida", 1.5f);
 
             Destroy(gameObject);
         }
